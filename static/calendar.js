96d40059b7c249cb027256056db2e10a4bb5059a
Ian <ianworks.my@gmail.com>
2025-05-09 19:45:24 +0800

    edited javascript

--- static/calendar.js
+++ static/calendar.js
@@ -3,23 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
     day = currentDate.getDay();
     day_idx = document.querySelectorAll('[data-index]');
     date = currentDate.getDate();
-
-    calendar_dates = this.getElementsByClassName("date")
-    console.log(calendar_dates);
     console.log(date);
 
-    for (let i = 0; i< day_idx.length; i++) {
-        const calendar_date = calendar_dates[i]
+    let calendar_dates = [];
+    date_div = document.getElementsByClassName("date");
 
-    }
+    let daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+    console.log(daysInMonth);
+
+    console.log(calendar_dates);
+    let emptyDaysBefore = day;
+    console.log(emptyDaysBefore);
 
     for (let i = 0; i< day_idx.length; i++) {
         const column = day_idx[i];
         const index = parseInt(column.getAttribute('data-index'));
+        calendar_date = calendar_dates[i];
 
         if (index === day) {
             // Change CSS style to highlight today's column
             column.style.backgroundColor = 'crimson';
+            
         }
     }
 
